002aee377f9c39ce12bb0e8fcebdca90b5992299
bleek42 <brandonleek42@gmail.com>
2021-09-19 11:30:28 -0400

    completed Meta.tsx component for SEO purposes; started Home.tsx page

---
+++ components/Meta.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+
+type MetaProps = {
+	title?: string;
+	description?: string;
+	keywords?: string;
+	image?: string | undefined;
+};
+
+const Meta = ({
+	title,
+	description,
+	keywords,
+	image,
+}: MetaProps): JSX.Element => {
+	return (
+		<Head>
+			<title>{title}</title>
+			<meta charSet="utf-8" />
+			<meta name="viewport" content="initial-scale=1, width=device-width" />
+			<meta name="keywords" content={keywords} />
+			<meta name="description" content={description} />
+			<meta property="og:title" content={title} />
+			<meta property="og:image" content={image ? image : undefined} />
+		</Head>
+	);
+};
+
+Meta.defaultProps = {
+	title: "bleek.tech",
+	description: "Brandon Leek - Full Stack Web Dev",
+	keywords:
+		"web development, application development, software, programming, react, node, sql, typescript",
+	image: undefined,
+};

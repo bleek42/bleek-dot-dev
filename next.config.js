ea943103a76ba2fdec0a75fb3dc09e7662c87595
bleek42 <brandonleek42@gmail.com>
2022-07-27 17:58:17 -0400

    correctly configured eslint, ts parser, prettier in nextjs app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
+  eslint: {
+    dirs: ['pages', 'components', 'constants', 'types']
+  }
 };

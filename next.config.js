318f73aa167138244ba45ab79e769c3d9a462993
bleek42 <brandonleek42@gmail.com>
2022-07-27 17:58:17 -0400

    correctly configured eslint, ts parser, prettier in nextjs app

--- next.config.js
+++ next.config.js
@@ -7,5 +7,11 @@ module.exports = {
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
+  eslint: {
+    dirs: ['pages', 'components', 'constants', 'types']
+  }
 };
 >>>>>>> b4d0830 (add prettier; replace apollo with gqlreq pkg; refactored some of the file structure, fixed errors in some components; generated new types techstack assets for graphcms)
